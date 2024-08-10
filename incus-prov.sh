d9f915b64fcd7222a0f5b896802d64416a915ce8
Derek Jewett <98997624+farmer1776@users.noreply.github.com>
2024-08-10 14:24:32 -0400

    Update incus-prov.sh
    cleanup

--- incus-prov.sh
+++ incus-prov.sh
@@ -3,7 +3,6 @@
 # Tested on Rocky Linux 9, Alma Linux 9, CentOS 9, Oracle9, Fedora40, Focal, Jammy, Noble, Bookworm
 while IFS=',' read -r name_c1 ip_c2 prof_c3 image_c4
 do
-    file=v1-$ip_c2.yaml
 incus launch images:$image_c4 "$name_c1" -p $prof_c3 -c cloud-init.network-config="version: 1
 config:
   - type: physical
